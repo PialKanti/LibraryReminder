b9752f28f463f5edfe1bfd3ec87c71c506d325e0
Pial Kanti Samadder <pialkanti2012@gmail.com>
2016-11-05 01:21:13 +0600

    Change Notification text and Add action button

--- app/src/main/java/pialkanti/com/libraryreminder/AlarmReceiver.java
+++ app/src/main/java/pialkanti/com/libraryreminder/AlarmReceiver.java
@@ -2,6 +2,7 @@ package pialkanti.com.libraryreminder;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,12 +17,18 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        Intent parentIntent = new Intent(context,UserHomeActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,0,parentIntent,0);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
         Notification notification = builder.setContentTitle("KUET Library Reminder")
                 .setContentText("You have a book to renew.")
                 .setTicker("Hurry")
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .addAction(R.mipmap.show_details,"Show details",pendingIntent)
                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.person))
                 .build();
 
